89b471b0828b31cb42cc67bb52cc72d3755afd3e
Alex <rabbit_1399@icloud.com>
2022-12-02 00:27:50 +0300

    refactor: some path changes

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -2,13 +2,13 @@ from typing import Any
 
 from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, filters
 
-from src.bot.admin.admins import admins_handler
-from src.bot.admin.callback_file import callback_file_handler
-from src.bot.admin.callback_message import callback_message_handler
-from src.bot.admin.callback_phone import callback_phone_handler
-from src.bot.admin.callback_query import callback_query_handler
-from src.bot.admin.mail import mail_handler
-from src.bot.admin.users import users_handler
+from src.bot.admin.handlers.admins import admins_handler
+from src.bot.admin.callbacks.callback_file import callback_file_handler
+from src.bot.admin.callbacks.callback_message import callback_message_handler
+from src.bot.admin.callbacks.callback_phone import callback_phone_handler
+from src.bot.admin.callbacks.callback_query import callback_query_handler
+from src.bot.admin.handlers.mail import mail_handler
+from src.bot.admin.handlers.users import users_handler
 from src.bot.help import help_handler
 from src.bot.start import start_handler
 
