5309e353f0be6167c24155a0d2b68576bd7c94e6
Alex <rabbit_1399@icloud.com>
2022-12-19 20:16:30 +0300

    bump: version 0.5.4 → 0.6.0

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION_PACKAGE=0.5.4
+VERSION_PACKAGE=0.6.0
 
 docker build -t tfs_notify:"$VERSION_PACKAGE" . && \
 docker run --network host -d --restart always -it tfs_notify:"$VERSION_PACKAGE"
